7199bd23a145f94cb8b6c40414322b5ef7db3ccb
jinh0park <jinh0park@naver.com>
2019-06-13 12:43:42 +0900

    yuufyg

--- dateapp/views.py
+++ dateapp/views.py
@@ -9,7 +9,8 @@ from .models import Spot
 
 
 def welcome(request):
-    return render(request, 'dateapp/welcome.html')
+    spots = Spot.objects.all()[:2]
+    return render(request, 'dateapp/welcome.html', {'spots':spots})
 
 
 @login_required
